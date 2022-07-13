8948f88917b13be729595e60408d63895a9fb5ba
Giovanni Guarnieri <gfguarnieri@hotmail.com>
2022-07-13 16:45:35 -0300

    fixed port

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,7 @@
 import { app } from '@shared/infra/http/app';
 
-app.listen(3333, () => {
-  console.log('Server started!');
+const port = process.env.PORT || 3333;
+
+app.listen(port, () => {
+  console.log(`Server started! PORT -> ${port}`);
 });
